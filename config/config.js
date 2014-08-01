b91935d42b4039f7a5cdd747769893e805affe98
rodekruis <mvanderveen@redcross.nl>
2014-08-01 11:19:10 +0200

    changes for ssl

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     usessl: true,
     sslport: 445, // 443?
+    mainPort: 3001,
     key_file: './config/cert/localhost-key.pem',
     cert_file: './config/cert/localhost-cert.pem'
 };
