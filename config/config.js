92d031168bf68a89917f460ba08dd5406ecc5bc3
rodekruis <mvanderveen@redcross.nl>
2014-10-02 13:08:50 +0200

    add ca file

--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ module.exports = {
     sslport: 445, // 443?
     mainPort: 3001,
     key_file: './config/cert/localhost-key.pem',
-    cert_file: './config/cert/localhost-cert.pem'
+    cert_file: './config/cert/localhost-cert.pem',
+    ca_file: './config/cert/thawte.ca'
 };
